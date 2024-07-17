3ce40b165db4e3f3b3436cf56820f1acee437948
Prahim_12 <ibrahim.mohamed@datagearbi.com>
2024-07-18 02:36:51 +0300

    test from ibrahim

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  constructor(){
+    console.log("Hello From prahim");
+  }
 }
