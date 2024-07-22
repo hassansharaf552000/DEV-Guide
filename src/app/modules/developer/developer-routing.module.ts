0256a0c66a80db04f2719014bcec5afb898cbf48
Adhamdy22 <adhomy125@gmail.com>
2024-07-22 19:48:49 +0300

    Adding (Mentor Card - Mentor List) & (Hr Card - Hr List)

--- src/app/modules/developer/developer-routing.module.ts
+++ src/app/modules/developer/developer-routing.module.ts
@@ -1,12 +1,28 @@
+import { RateComponent } from './Components/rate/rate.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { MentorListComponent } from './Components/mentor-list/mentor-list.component';
+import { MentorCardComponent } from './Components/mentor-card/mentor-card.component';
+import { HRCardComponent } from './Components/hr-card/hr-card.component';
+import { HRListComponent } from './Components/hr-list/hr-list.component';
 
-
+const routes: Routes = [
+  { path: 'mentors', component: MentorListComponent},
+  {path: 'HR-S', component: HRListComponent}
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MentorListComponent,
+    MentorCardComponent,
+    RateComponent,
+    HRListComponent,
+    HRCardComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    [RouterModule.forChild(routes)]
+  ],
+  exports: [RouterModule]
 })
 export class DeveloperRoutingModule { }
