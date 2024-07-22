6e48849a03e2124ccb887c966a4f3d11b091d507
Mohamed1995Ahmed <engmohamedabdelhakem@gmail.com>
2024-07-22 08:29:18 +0200

    add

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common'
+//import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
@@ -17,7 +17,7 @@ import { Update_profileComponent } from './components/update_profile/update_prof
     Update_profileComponent
   ],
   imports: [
-    CommonModule
+   // CommonModule
   ]
 })
 export class SharedModule { }
