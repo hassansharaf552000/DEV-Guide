884219706c58cd13e4131755776bc5e05713703e
hassansharaf552000 <hassansharaf54200022@gmail.com>
2024-09-08 11:55:47 +0300

    finishing booking and mentor payment

--- src/app/modules/mentor/mentor.module.ts
+++ src/app/modules/mentor/mentor.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MentorRoutingModule } from './mentor-routing.module';
-
-
+import { MentorPaymentsComponent } from './components/mentor-payments/mentor-payments.component';
+import { MentorlayoutComponent } from './components/mentorlayout/mentorlayout.component';
+import { SidebarlayoutComponent } from './components/sidebarlayout/sidebarlayout.component';
+import { SharedModule } from '../../shared/shared.module';
+import { BookingComponent } from './components/booking/booking.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    MentorRoutingModule
-  ]
+  declarations: [
+    MentorPaymentsComponent,
+    MentorlayoutComponent,
+    SidebarlayoutComponent,
+    BookingComponent,
+  ],
+  imports: [CommonModule, MentorRoutingModule, SharedModule],
 })
-export class MentorModule { }
+export class MentorModule {}
