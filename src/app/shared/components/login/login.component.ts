5df3d12a55f5b4fb5df91470c6e82192c572934a
Adhamdy22 <adhomy125@gmail.com>
2024-10-07 10:40:22 +0300

    Make Ui Of Login

--- src/app/shared/components/login/login.component.ts
+++ src/app/shared/components/login/login.component.ts
@@ -7,5 +7,23 @@ import { Router } from '@angular/router';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    // Initialize the form
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      rememberMe: [false]
+    });
+  }
+
+  // Method to handle form submission
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form Submitted:', this.loginForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
