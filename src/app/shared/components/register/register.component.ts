215244e0bd513338e31c8324fe1fb94bc53385cc
DinaJohn8 <dinajohnaziz@gmail.com>
2024-10-07 18:59:18 +0300

    register

--- src/app/shared/components/register/register.component.ts
+++ src/app/shared/components/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../services/Auth/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -9,19 +10,59 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  form:FormGroup
-  constructor(private router:Router,private builder:FormBuilder){
+  form:FormGroup;
+   returnUrl='/developer/step-one'
+  constructor(private router:Router,private builder:FormBuilder, private authService: AuthService ){
     this.form = this.builder.group({
-      email: ["", [Validators.required, Validators.email]],
-      personname:["",[Validators.required,Validators.minLength(20)]],
-      username:["",[Validators.required,Validators.pattern(/^\S*$/),Validators.minLength(8)]],
-      phonenumber:["",[Validators.required,Validators.pattern(/^\d{11}$/)]],
-      password: ["", [Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,16}$/)]],
+      Email: ["", [Validators.required, Validators.email]],
+      FirstName:["",[Validators.required,Validators.minLength(4)]],
+      LastName:["",[Validators.required,Validators.minLength(4)]],
+
+      UserName:["",[Validators.required,Validators.pattern(/^\S*$/),Validators.minLength(8)]],
+      // phonenumber:["",[Validators.required,Validators.pattern(/^\d{11}$/)]],
+
+      Password: ["", [Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,16}$/)]],
+      ConfirmPassword: ["", [Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,16}$/)]],
+      Role: [''],
+      //Role: ['', Validators.required]
+
     
   })
 }
-send() {
-  console.log(this.form.value);
+// send() {
+//   console.log(this.form.value);
+// }
+setRole(Role: string) {
+  this.form.patchValue({ Role: Role });
 }
 
+// register() {
+//   if (this.form.valid) {
+//     this.authService.register(this.form.value).subscribe({
+//       next: (response) => console.log(response),
+//       error: (error) => console.error(error)
+//     });
+//   }
+// }
+// }
+send() {
+  console.log(this.form.value);
+
+  this.authService.register(this.form.value).subscribe({
+    next:(res:any)=>{
+      console.log(res);
+      
+      if(res){
+      alert("registered successfully");
+        this.router.navigateByUrl(this.returnUrl)
+
+      }
+      
+    },
+    error:(err)=>{
+      console.log(err);
+      alert("Sorry try again leter")
+    }
+  })
+}
 }
