497c2a7bd3aa381edd6c03b78df4eb9825c8e954
Mirna-Alfy <mernaalfy30@gmail.com>
2024-10-22 11:29:35 +0300

    schedule

---
+++ src/app/shared/services/Schedule/schedule.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ScheduleService {
+
+  SetScheduleURL="http://localhost:5164/api/Schedule/CheckAndHandleSchedule"
+  GetUserPriceURL="http://localhost:5164/api/Schedule/GetUserPrice"
+  UpdateUserPriceURL="http://localhost:5164/api/Schedule/UpdateUserPrice"
+
+
+  constructor(private http: HttpClient) { }
+
+  SetSchedule(Schedule:any) {
+    return this.http.post(this.SetScheduleURL,Schedule)
+  }
+  GetUserPrice(){
+    return this.http.get(this.GetUserPriceURL)
+  }
+  
+  UpdateUserPrice(Price:any): Observable<any>  {
+    return this.http.post(this.UpdateUserPriceURL,Price)
+  }
+
+}
